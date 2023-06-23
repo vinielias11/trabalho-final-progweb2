218076bfc51855f62597d8159cc97844c463a531
Leonardo-DeLuca <leonardo.s.deluca@gmail.com>
2023-06-23 20:30:32 -0300

    finalizado realizar testes

--- front/src/components/realizaTeste/index.js
+++ front/src/components/realizaTeste/index.js
@@ -7,19 +7,22 @@ const RealizaTestes = ({realizaTeste}) => {
     const [exibirBotaoFinalizar, setBotaoFinalizar] = useState(false);
     const [perguntaAtual, setPergunta] = useState(realizaTeste.perguntas[indicePerguntaAtual]);
     const [altMarcada, setAltMarcada] = useState('')
+    const [isFinalizado, setFinalizado] = useState(false)
+    const [perguntasCorretas, setPerguntasCorretas] = useState(0)
     const [autenticado] = AuthConsumer()
 
     useEffect(() => {
-
+        
+        setAltMarcada("");
         setPergunta(realizaTeste.perguntas[indicePerguntaAtual]);
 
-        if (indicePerguntaAtual + 1 === realizaTeste.perguntas.length) {
+        if (indicePerguntaAtual + 1 === realizaTeste.perguntas.length && !isFinalizado) {
             setBotaoFinalizar(true);
           } else {
             setBotaoFinalizar(false);
           }
 
-      }, [realizaTeste.perguntas, indicePerguntaAtual]);
+      }, [realizaTeste.perguntas, indicePerguntaAtual, isFinalizado]);
 
     
     
@@ -41,11 +44,15 @@ const RealizaTestes = ({realizaTeste}) => {
     const handleClick = (alt) => {
         setAltMarcada(alt)
         perguntaAtual.alternativaMarcada = alt;
+
     }
 
 
     const onClickFinalizarTeste = () => {
         criarResultado();
+        setFinalizado(true);
+        setBotaoFinalizar(false);
+        alert("Teste finalizado e resultados computados!")
     }
 
     const criarResultado = async () => {
@@ -75,6 +82,8 @@ const RealizaTestes = ({realizaTeste}) => {
 
         let porcentagemDeAcertos = ((perguntasCorretas / realizaTeste.perguntas.length) * 100).toFixed(2);
 
+        setPerguntasCorretas(perguntasCorretas);
+
         return {
             idTeste: realizaTeste._id,
             nomeTeste: realizaTeste.nome,
@@ -83,8 +92,53 @@ const RealizaTestes = ({realizaTeste}) => {
             respostasCorretas: perguntasCorretas,
             porcentagemAcertos: porcentagemDeAcertos,
         }
+
     }
 
+    const getEstiloA =  () => {
+        if (perguntaAtual.alternativaMarcada !== perguntaAtual.alternativaCorreta && isFinalizado && perguntaAtual.alternativaMarcada === 'A') {
+            return {backgroundColor: 'red', borderColor: 'red'};
+        }else if(perguntaAtual.alternativaCorreta === 'A' && isFinalizado){
+            return {backgroundColor: 'green', borderColor: '#41b10e', color: 'white'}
+        }
+        return {}
+    }
+    const getEstiloB = () => {
+        if (perguntaAtual.alternativaMarcada !== perguntaAtual.alternativaCorreta && isFinalizado && perguntaAtual.alternativaMarcada === 'B') {
+            return {backgroundColor: 'red', borderColor: 'red'};
+        }else if(perguntaAtual.alternativaCorreta === 'B' && isFinalizado){
+            return {backgroundColor: 'green', borderColor: '#41b10e', color: 'white'}
+        }
+
+        return {}
+    }
+    const getEstiloC = () => {
+        if (perguntaAtual.alternativaMarcada !== perguntaAtual.alternativaCorreta && isFinalizado && perguntaAtual.alternativaMarcada === 'C') {
+            return {backgroundColor: 'red', borderColor: 'red'};
+        }else if(perguntaAtual.alternativaCorreta === 'C' && isFinalizado){
+            return {backgroundColor: 'green', borderColor: '#41b10e', color: 'white'}
+        }
+
+        return {}
+    }
+    const getEstiloD = () => {
+        if (perguntaAtual.alternativaMarcada !== perguntaAtual.alternativaCorreta && isFinalizado && perguntaAtual.alternativaMarcada === 'D') {
+            return {backgroundColor: 'red', borderColor: 'red'};
+        }else if(perguntaAtual.alternativaCorreta === 'D' && isFinalizado){
+            return {backgroundColor: 'green', borderColor: '#41b10e', color: 'white'}
+        }
+
+        return {}
+    }
+    const getEstiloE = () => {
+        if (perguntaAtual.alternativaMarcada !== perguntaAtual.alternativaCorreta && isFinalizado && perguntaAtual.alternativaMarcada === 'E') {
+            return {backgroundColor: 'red', borderColor: 'red'};
+        }else if(perguntaAtual.alternativaCorreta === 'E' && isFinalizado){
+            return {backgroundColor: 'green', borderColor: '#41b10e', color: 'white'}
+        }
+
+        return {}
+    }
 
     return (
             <>
@@ -104,19 +158,19 @@ const RealizaTestes = ({realizaTeste}) => {
                             <h4>Resposta</h4>
                         </div>
                         <div className="realiza_row realiza_linha_alternativa">
-                            <input id="alternativaA" type="radio" name="alternativas" value="A" checked={perguntaAtual.alternativaMarcada === 'A'} onChange={(event) => handleClick(event.target.value)} /> <label htmlFor="alternativaA" className="realiza_letraAlternativa">A</label>
+                            <input id="alternativaA" type="radio" name="alternativas" value="A" disabled={isFinalizado} checked={perguntaAtual.alternativaMarcada === 'A'} onChange={(event) => handleClick(event.target.value)} /> <label style={getEstiloA()} htmlFor="alternativaA" className="realiza_letraAlternativa">A</label>
                             <input type="radio" name="tituloAlternativaA" disabled={true} /><label htmlFor="tituloAlternativaA" className="realiza_tituloAlternativas" id="tituloAlternativaA">{perguntaAtual.alternativaA}</label>
                         </div><div className="realiza_row realiza_linha_alternativa">
-                            <input id="alternativaB" type="radio" name="alternativas" value="B" checked={perguntaAtual.alternativaMarcada === 'B'} onChange={(event) => handleClick(event.target.value)} /> <label htmlFor="alternativaB" className="realiza_letraAlternativa">B</label>
+                            <input id="alternativaB" type="radio" name="alternativas" value="B" disabled={isFinalizado} checked={perguntaAtual.alternativaMarcada === 'B'} onChange={(event) => handleClick(event.target.value)} /> <label style={getEstiloB()} htmlFor="alternativaB" className="realiza_letraAlternativa">B</label>
                             <input type="radio" name="tituloAlternativaB" disabled={true} /><label htmlFor="tituloAlternativaB" className="realiza_tituloAlternativas" id="tituloAlternativaB">{perguntaAtual.alternativaB}</label>
                         </div><div className="realiza_row realiza_linha_alternativa">
-                            <input id="alternativaC" type="radio" name="alternativas" value="C" checked={perguntaAtual.alternativaMarcada === 'C'} onChange={(event) => handleClick(event.target.value)} /> <label htmlFor="alternativaC" className="realiza_letraAlternativa">C</label>
+                            <input id="alternativaC" type="radio" name="alternativas" value="C" disabled={isFinalizado} checked={perguntaAtual.alternativaMarcada === 'C'} onChange={(event) => handleClick(event.target.value)} /> <label style={getEstiloC()} htmlFor="alternativaC" className="realiza_letraAlternativa">C</label>
                             <input type="radio" name="tituloAlternativaC" disabled={true} /><label htmlFor="tituloAlternativaC" className="realiza_tituloAlternativas" id="tituloAlternativaC">{perguntaAtual.alternativaC}</label>
                         </div><div className="realiza_row realiza_linha_alternativa">
-                            <input id="alternativaD" type="radio" name="alternativas" value="D" checked={perguntaAtual.alternativaMarcada === 'D'} onChange={(event) => handleClick(event.target.value)} /> <label htmlFor="alternativaD" className="realiza_letraAlternativa">D</label>
+                            <input id="alternativaD" type="radio" name="alternativas" value="D" disabled={isFinalizado} checked={perguntaAtual.alternativaMarcada === 'D'} onChange={(event) => handleClick(event.target.value)} /> <label style={getEstiloD()} htmlFor="alternativaD" className="realiza_letraAlternativa">D</label>
                             <input type="radio" name="tituloAlternativaD" disabled={true} /><label htmlFor="tituloAlternativaD" className="realiza_tituloAlternativas" id="tituloAlternativaD">{perguntaAtual.alternativaD}</label>
                         </div><div className="realiza_row realiza_linha_alternativa">
-                            <input id="alternativaE" type="radio" name="alternativas" value="E" checked={perguntaAtual.alternativaMarcada === 'E'} onChange={(event) => handleClick(event.target.value)} /> <label htmlFor="alternativaE" className="realiza_letraAlternativa">E</label>
+                            <input id="alternativaE" type="radio" name="alternativas" value="E" disabled={isFinalizado} checked={perguntaAtual.alternativaMarcada === 'E'} onChange={(event) => handleClick(event.target.value)} /> <label style={getEstiloE()} htmlFor="alternativaE" className="realiza_letraAlternativa">E</label>
                             <input type="radio" name="tituloAlternativaE" disabled={true} /><label htmlFor="tituloAlternativaE" className="realiza_tituloAlternativas" id="tituloAlternativaE">{perguntaAtual.alternativaE}</label>
                         </div>
                     </form>
@@ -129,6 +183,12 @@ const RealizaTestes = ({realizaTeste}) => {
                 <h4 id="numeroPerguntas">Pergunta {indicePerguntaAtual + 1} / {realizaTeste.perguntas.length}</h4>
                 {exibirBotaoFinalizar && (
                     <button id="btnFinalizar" className="realiza_btn" onClick={() => onClickFinalizarTeste()}><i className="fa fa-check"></i>Finalizar</button>
+                )}
+                {isFinalizado && (
+                    <>
+                        <h4 id="porcentagemAcertos">Porcentagem de Acertos: {((perguntasCorretas / realizaTeste.perguntas.length) * 100).toFixed(2)}%</h4>
+                        <h4>Quantidade de Acertos: {perguntasCorretas} / {realizaTeste.perguntas.length}</h4>
+                    </>
                 )}
                 <h4 id="realiza_porcentagemAcertos" className="realiza_porcentagemAcertos"></h4>
             </div></>
